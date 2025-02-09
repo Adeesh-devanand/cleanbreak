db2716521f3b2112e4e76c30807c07c27b6f9b2f
Adeesh Devanand <yorox579@gmail.com>
2025-02-09 02:41:46 -0800

    Initial Commit

---
+++ cleanbreakTests/cleanbreakTests.swift
@@ -0,0 +1,16 @@
+//
+//  cleanbreakTests.swift
+//  cleanbreakTests
+//
+//  Created by user270007 on 2/9/25.
+//
+
+import Testing
+
+struct cleanbreakTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
